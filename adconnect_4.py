15b7c70e658cacadea39c272a148717f5bf8bfa2
mruchus <mruchus@icloud.com>
2020-07-31 15:54:51 +0200

    initial version working

--- adconnect_4.py
+++ adconnect_4.py
@@ -1,10 +1,20 @@
 import numpy as np
+import pygame
+import sys
+import math
+
+# constants
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
 
 ROW_COUNT = 6
 COLUMN_COUNT = 7
 
-# creating a board: 6 rows by seven columns, filled with zeros, array form
+#functions
 
+# creating a board: 6 rows by seven columns, filled with zeros, array form
 def create_board():
     board = np.zeros((ROW_COUNT,COLUMN_COUNT))
     return board
@@ -27,9 +37,11 @@ def get_next_open_row(board,col):
             # returns the row number in which the row contains a 0
             return r
 
+
 def print_board(board):
     # flipping the board so (0,0) will be at the bottom left
     print(np.flip(board, 0))
+
 
 def winning_move(board, piece):
     # checking for horizontal wins
@@ -62,6 +74,26 @@ def winning_move(board, piece):
                 return True
 
 
+def draw_board(board):
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+
+            pygame.draw.rect(screen, BLUE, (c*SQUARESIZE, r*SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
+            pygame.draw.circle(screen, BLACK, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), RADIUS )
+
+
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+
+            if board[r][c] == 1:
+                pygame.draw.circle(screen, RED, (int(c*SQUARESIZE+SQUARESIZE/2), height - int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS )
+
+            elif board[r][c] == 2:
+                pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), height - int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS )
+
+
+    pygame.display.update()
+
 
 
 # game doesn't end until someone wins and so it turns True
@@ -73,37 +105,104 @@ game_over = False
 turn = 0
 
 
-while not game_over:
-    # ask for Player 1's input - where do they want to go?
-    if turn == 0:
+pygame.init()
 
-        col = int(input("Player 1, make your selection (0-6):"))
+SQUARESIZE = 100
 
-        if is_valid_location(board, col):
-            row = get_next_open_row(board,col)
-            drop_piece(board, row, col, 1)
+width = COLUMN_COUNT * SQUARESIZE
+height = (ROW_COUNT + 1) * SQUARESIZE
+
+size = (width, height)
+
+RADIUS = int(SQUARESIZE/2 - 5)
+
+screen = pygame.display.set_mode(size)
+draw_board(board)
+pygame.display.update()
+
+myfont = pygame.font.SysFont("monospace", 60)
 
 
-            if winning_move(board, 1):
+
+# main program
+
+while True:
+
+    while not game_over:
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            if event.type == pygame.MOUSEMOTION:
+                pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+                posx = event.pos[0]
+
+                if turn == 0:
+                    pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
+
+                else:
+                    pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
+
+            pygame.display.update()
+
+
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+
+                if turn == 0:
+                    posx =event.pos[0]
+                    col = int(math.floor(posx / SQUARESIZE))
+
+                    if is_valid_location(board, col):
+                        pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
+
+                        if turn == 0:
+                            row = get_next_open_row(board, col)
+                            drop_piece(board, row, col, 1)
+
+                            if winning_move(board, 1):
+                                label = myfont.render("Player 1 WINS!", 1, RED)
+                                screen.blit(label, (40,10))
+                                game_over = True
+
+                #ask for player 2 input
+                else:
+                    posx = event.pos[0]
+                    col = int(math.floor(posx / SQUARESIZE))
+
+                    if is_valid_location(board, col):
+                        row = get_next_open_row(board, col)
+                        drop_piece(board, row, col, 2)
+
+                        if winning_move(board, 2):
+                            label = myfont.render("Player 2 WINS!", 1, YELLOW)
+                            screen.blit(label, (40,10))
+                            game_over =  True
+
+
+
                 print()
-                print(" Player 1, YOU WIN!")
-                game_over = True
+                print_board(board)
+                draw_board(board)
+                print()
 
-    #ask for player 2 input
-    else:
+                turn += 1
+                turn = turn % 2
 
-        col = int(input("Player 2, make your selection (0-6):"))
+    #start again
+    game_over = False
+    board = create_board()
+    draw_board(board)
 
-        if is_valid_location(board, col):
-            row = get_next_open_row(board,col)
-            # note: '2' is taken in as the 'piece' here
-            drop_piece(board, row, col, 2)
 
-    print()
-    print_board(board)
-    print()
 
-    turn += 1
-    turn = turn % 2
+
+
+
+
+
 
 
---
+++ connect_4.py
@@ -0,0 +1,48 @@
+
+#TO-DO LIST
+# - Create Board
+# - Choose players and Initial player
+# - Play until someone wins
+# - Checking for the actual winner
+# - Show Board
+# - Choose where to mark the board
+# - Winner or Draw?
+# - Game over
+
+
+def main():
+    print()
+    print("You are playing 'CONNECT_4' ...")
+    print()
+
+#CREATING THE BOARD
+#Board is a list of rows
+#Rows is a list of cells
+
+    board = [
+
+        
+        [None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None],
+
+
+    ]
+
+    # CHOOSE INITIAL PLAYER
+    #
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
